e99ed3438f1416fa8523188152d86f4442fdf22a
pessoaB321 <clari.d.moura499@gmail.com>
2026-09-23 18:17:10 -0300

    Delete src

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import Politico from "./politico";
-import Governador from "./governador";
-import Precidente from "./precidente";
-import DEPUestaDU from "./deputadoEst";
-import FeDedePu from "./deputadoFED";
-import Senadoro  from "./Senador";
